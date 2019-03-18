c067d76468986d269977828dd3cfb0357eb0ce70
Anson Gurung <gurunga4@cardiff.ac.uk>
2019-03-18 21:16:11 +0000

    Implemented A Map Fragment which has a hard coded Lat-Longitude co-ordinates for Cardiff

--- app/src/main/java/com/example/animalapp/MainActivity.java
+++ app/src/main/java/com/example/animalapp/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.animalapp;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, new MapsActivity())
+                .commit();
     }
 }
---
+++ app/src/main/java/com/example/animalapp/MapsActivity.java
@@ -0,0 +1,60 @@
+package com.example.animalapp;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_maps, container, false);
+
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        SupportMapFragment fragment = new SupportMapFragment();
+        transaction.add(R.id.map, fragment);
+        transaction.commit();
+        fragment.getMapAsync(this);
+
+
+
+        return v;
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng cardiff = new LatLng(51.481583, -3.179090);
+        mMap.addMarker(new MarkerOptions().position(cardiff).title("Marker in Cardiff"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(cardiff));
+    }
+}
