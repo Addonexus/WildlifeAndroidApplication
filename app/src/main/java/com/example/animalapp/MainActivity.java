3d5de66b385383299b4bf8bc64d1f6f7f178ea04
Anson Gurung <gurunga4@cardiff.ac.uk>
2019-03-19 05:55:56 +0000

    Implemented a Bottom Nav Bar with 5 Buttons which show a Toast Message when tapped

--- app/src/main/java/com/example/animalapp/MainActivity.java
+++ app/src/main/java/com/example/animalapp/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.animalapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView =  findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+
+                        break;
+                    case R.id.action_animals:
+                        Toast.makeText(MainActivity.this, "Animals", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_map:
+                        Toast.makeText(MainActivity.this, "Map", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_reserves:
+                        Toast.makeText(MainActivity.this, "Nature Reserve", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_links:
+                        Toast.makeText(MainActivity.this, "External Links", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+                return true;
+            }
+
+        });
     }
 }
