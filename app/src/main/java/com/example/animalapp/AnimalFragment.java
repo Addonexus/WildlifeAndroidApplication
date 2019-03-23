ab1d3aa0f6fefdfa9e456d99077fff6ff124986d
Muhammad Ahmad <hajiahmadmm@cardiff.ac.uk>
2019-03-23 15:16:45 +0000

    added fragment page for animals

---
+++ app/src/main/java/com/example/animalapp/AnimalFragment.java
@@ -0,0 +1,34 @@
+package com.example.animalapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AnimalFragment extends Fragment {
+
+    private View v;
+
+
+    public AnimalFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        v =  inflater.inflate(R.layout.fragment_animal, container, false);
+
+        return v;
+    }
+
+
+}
