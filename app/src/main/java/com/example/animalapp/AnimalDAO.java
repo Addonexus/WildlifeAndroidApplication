772bd79882621ed129512a2034f9b1b20a9ced27
Muhammad Ahmad <hajiahmadmm@cardiff.ac.uk>
2019-03-24 17:33:48 +0000

    added basic ui for animal fragment

--- app/src/main/java/com/example/animalapp/AnimalDAO.java
+++ app/src/main/java/com/example/animalapp/AnimalDAO.java
@@ -24,6 +24,16 @@ public interface AnimalDAO {
     @Query("SELECT * FROM Animal WHERE type LIKE :amphibian")
     List<Animal> getAllAmphibian(String amphibian);
 
+    @Query("SELECT * FROM Animal WHERE type LIKE :bird " +
+            "AND `Min Body Length Cm` <= :minBody " +
+            "AND `Min Wingspan Cm` <= :minWing")
+    List<Animal> getBirdMinMin(String bird, int minBody, int minWing);
+
+    @Query("SELECT * FROM Animal WHERE type LIKE :bird " +
+            "AND `Min Body Length Cm` <= :minBody " +
+            "AND `Max Wingspan Cm` >= :maxWing")
+    List<Animal> getBirdMinMax(String bird, int minBody, int maxWing);
+
     @Insert
     void insertAll(Animal... animals);
 
