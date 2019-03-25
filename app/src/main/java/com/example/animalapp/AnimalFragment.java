25e6788643c43c48f757b43ef3a3086ebcac3fe6
Anson Gurung <gurunga4@cardiff.ac.uk>
2019-03-25 03:51:20 +0000

    Implemented Species Identifier Button in Animal Fragment that Directs to a List of Buttons of Species Types starting nev graph

--- app/src/main/java/com/example/animalapp/AnimalFragment.java
+++ app/src/main/java/com/example/animalapp/AnimalFragment.java
@@ -4,15 +4,19 @@ package com.example.animalapp;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.navigation.Navigation;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AnimalFragment extends Fragment {
+public class AnimalFragment extends Fragment implements View.OnClickListener{
 
 
     public AnimalFragment() {
@@ -24,7 +28,26 @@ public class AnimalFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_animal_dictionary, container, false);
+        View view = inflater.inflate(R.layout.fragment_animal_dictionary, container, false);
+        Button species_identifier_btn = (Button) view.findViewById(R.id.species_identifier_button);
+        species_identifier_btn.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        Fragment fragment = null;
+        int i = v.getId();
+        if (i == R.id.species_identifier_button) {
+            fragment = new SpeciesIdentifier();
+            replaceFragment(fragment);
+
+        }
+    }
+    public void replaceFragment(Fragment someFragment) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame, someFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 }
