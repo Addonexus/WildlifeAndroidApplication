96937bc6861f0deb586ec5e12c078f85efb33163
Anson Gurung <gurunga4@cardiff.ac.uk>
2019-03-25 05:34:26 +0000

    Implemented Values being passed through two question fragments for the Bird Search, Displaying the chosen options at the end

--- app/src/main/java/com/example/animalapp/SpeciesIdentifierResult.java
+++ app/src/main/java/com/example/animalapp/SpeciesIdentifierResult.java
@@ -3,15 +3,22 @@ package com.example.animalapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SpeciesIdentifierResult extends Fragment {
+    ArrayList<String> filterItems = new ArrayList<>();
 
 
     public SpeciesIdentifierResult() {
@@ -23,7 +30,22 @@ public class SpeciesIdentifierResult extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_species_identifier_result, container, false);
+        View view = inflater.inflate(R.layout.fragment_species_identifier_result, container, false);
+        Bundle bundle = this.getArguments();
+        TextView passed_detail = view.findViewById(R.id.species_identifier_result_text);
+        if (bundle != null) {
+            filterItems = bundle.getStringArrayList("filter");
+
+            StringBuilder filter = new StringBuilder();
+            for (String filterItem :
+                    filterItems) {
+                Log.i("PASSED VALUES: ", filterItem);
+                List<String> splitFilterItem = Arrays.asList(filterItem.split(":"));
+                filter.append(splitFilterItem.get(0)).append(":").append(splitFilterItem.get(1)).append(" ");
+            }
+            passed_detail.setText("Filter: " + filter);
+        }
+        return view;
     }
 
 }
