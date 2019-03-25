85eb4afa4484103998f9d0eeabe77ae9acc97ce3
Muhammad Ahmad <hajiahmadmm@cardiff.ac.uk>
2019-03-25 21:39:48 +0000

    added reference to code

--- app/src/main/java/com/example/animalapp/MainActivity.java
+++ app/src/main/java/com/example/animalapp/MainActivity.java
@@ -58,3 +58,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
+/*
+*   Read CSV Resource File
+*   Taken from Youtube by Brian Fraser 26/02/17
+*   Accessed 25/03/2019
+*   https://www.youtube.com/watch?v=i-TqNzUryn8
+*   InputStream is = getResources().openRawResource(R.raw.animal_list);
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(is, Charset.forName("UTF-8"))
+        );
+
+        String line = "";
+        try {
+            while ((line = br.readLine()) != null) {
+                String[] tokens = line.split(",");
+
+                Animal animal = new Animal(tokens[0], tokens[1], tokens[2],
+                        Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]),
+                        Integer.valueOf(tokens[5]),Integer.valueOf(tokens[6]),
+                        tokens[7], tokens[8], tokens[9], tokens[10],tokens[11]);
+
+                animalList.add(animal);
+
+                Log.d("Activity: ", "Animal List: " + animal);
+
+            }
+        } catch (IOException e) {
+            Log.wtf("Opening File", "Error reading file." + line, e);
+            e.printStackTrace();
+        }
+*
+*/
