131bde2d2c01a4240ba44134f3d7959ae3d8d5dc
Anson Gurung <GurungA4@cardiff.ac.uk>
2019-04-01 03:38:36 +0100

    Implemented Extra navigation in nav graph to skip a question and added Skip button on Choose Species Fragment

--- app/src/main/java/com/example/animalapp/ChooseSpecies.java
+++ app/src/main/java/com/example/animalapp/ChooseSpecies.java
@@ -30,6 +30,13 @@ public class ChooseSpecies extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_choose_species, container, false);
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            bundle.remove("BirdHeight");
+            bundle.remove("BirdColour");
+            bundle.remove("SpeciesType");
+
+        }
         Button species_bird_btn = (Button) view.findViewById(R.id.species_bird_button);
         Button species_mammal_btn = (Button) view.findViewById(R.id.species_mammal_button);
         Button species_reptile_btn = (Button) view.findViewById(R.id.species_reptile_button);
@@ -38,6 +45,12 @@ public class ChooseSpecies extends Fragment implements View.OnClickListener {
         species_mammal_btn.setOnClickListener(this);
         species_reptile_btn.setOnClickListener(this);
         species_invertebrate_btn.setOnClickListener(this);
+
+
+        Button species_back_btn = (Button) view.findViewById(R.id.species_back_button);
+        Button species_skip_btn = (Button) view.findViewById(R.id.species_skip_button);
+        species_back_btn.setOnClickListener(this);
+        species_skip_btn.setOnClickListener(this);
 
 
 
@@ -54,14 +67,22 @@ public class ChooseSpecies extends Fragment implements View.OnClickListener {
             filterItems.add("Type" + ":" + "Bird");
 
             bundle.putStringArrayList("filter", filterItems);
+            bundle.putString("SpeciesType", "Bird");
             Navigation.findNavController(v).navigate(R.id.action_chooseSpecies_to_birdHeightFragment,bundle);
 
         }
+        if (i == R.id.species_back_button) {
+            SpeciesIdentifier fragment = new SpeciesIdentifier();
+            replaceFragment(new SpeciesIdentifier());
+        }
+        if (i == R.id.species_skip_button) {
+            bundle.remove("SpeciesType");
+            Navigation.findNavController(v).navigate(R.id.action_chooseSpecies_to_birdHeadColourFragment,bundle);
+        }
     }
     public void replaceFragment(Fragment someFragment) {
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame, someFragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame, someFragment);
+        fragmentTransaction.commit();
     }
 }
